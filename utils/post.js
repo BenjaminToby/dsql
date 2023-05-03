cd22ca442d4cb71e0d055d0156eab83b6d1c16f7
Tben <52448020+BenjaminToby@users.noreply.github.com>
2023-05-03 07:16:29 +0100

    first commit

---
+++ utils/post.js
@@ -0,0 +1,84 @@
+/**
+ * ==============================================================================
+ * Imports
+ * ==============================================================================
+ */
+const https = require("https");
+
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+
+/**
+ * ==============================================================================
+ * Main Function
+ * ==============================================================================
+ * @param {String} key - API Key
+ * @param {String | Object} payload - SQL query String or Request Object. Eg. {
+        action: "insert | update | delete",
+        data: {
+            user_id: user.id,
+            user_first_name: user.first_name,
+            user_last_name: user.last_name,
+        },
+        table: "posts",
+    }
+ */
+module.exports = async function ({ key, payload }) {
+    /**
+     * Make https request
+     *
+     * @description make a request to datasquirel.com
+     */
+    const httpResponse = await new Promise((resolve, reject) => {
+        https
+            .request(
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: key,
+                    },
+                    port: 443,
+                    hostname: "datasquirel.com",
+                    path: `/api/query/post`,
+                },
+
+                /**
+                 * Callback Function
+                 *
+                 * @description https request callback
+                 */
+                (response) => {
+                    var str = "";
+
+                    response.on("data", function (chunk) {
+                        str += chunk;
+                    });
+
+                    response.on("end", function () {
+                        resolve(JSON.parse(str));
+                    });
+
+                    response.on("error", (err) => {
+                        reject(err);
+                    });
+                }
+            )
+            .write(payload)
+            .end();
+    });
+
+    /** ********************************************** */
+    /** ********************************************** */
+    /** ********************************************** */
+
+    return httpResponse;
+};
+
+/** ********************************************** */
+/** ********************************************** */
+/** ********************************************** */
---
+++ utils/upload-image.js
@@ -0,0 +1,81 @@
+/**
+ * ==============================================================================
+ * Imports
+ * ==============================================================================
+ */
+const https = require("https");
+
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+/** ****************************************************************************** */
+
+/**
+ * ==============================================================================
+ * Main Function
+ * ==============================================================================
+ * @param {String} key - API Key
+ * @param {String} payload - Image Data Eg. {
+        imageData: imageBase64,
+        imageName: `cast_cord_user_${newUser.payload.insertId}`,
+        mimeType: "jpg",
+        thumbnailSize: 120,
+    }
+ */
+module.exports = async function ({ key, payload }) {
+    /**
+     * Make https request
+     *
+     * @description make a request to datasquirel.com
+     */
+    const httpResponse = await new Promise((resolve, reject) => {
+        https
+            .request(
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: key,
+                    },
+                    port: 443,
+                    hostname: "datasquirel.com",
+                    path: `/api/query/post`,
+                },
+
+                /**
+                 * Callback Function
+                 *
+                 * @description https request callback
+                 */
+                (response) => {
+                    var str = "";
+
+                    response.on("data", function (chunk) {
+                        str += chunk;
+                    });
+
+                    response.on("end", function () {
+                        resolve(JSON.parse(str));
+                    });
+
+                    response.on("error", (err) => {
+                        reject(err);
+                    });
+                }
+            )
+            .write(payload)
+            .end();
+    });
+
+    /** ********************************************** */
+    /** ********************************************** */
+    /** ********************************************** */
+
+    return httpResponse;
+};
+
+/** ********************************************** */
+/** ********************************************** */
+/** ********************************************** */
