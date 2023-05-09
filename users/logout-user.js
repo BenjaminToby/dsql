1c690339d1751bb899b6780b80fdbf955df4cb83
Tben <52448020+BenjaminToby@users.noreply.github.com>
2023-05-09 15:20:53 +0100

    updates

--- users/logout-user.js
+++ users/logout-user.js
@@ -4,22 +4,42 @@
  * ==============================================================================
  * @param {Object} response - Http response object
  */
-module.exports = function ({ response }) {
+module.exports = function ({ request, response }) {
     /**
      * Check Encryption Keys
      *
      * @description Check Encryption Keys
      */
-    response.setHeader("Set-Cookie", ["datasquirelAuthKey=null;samesite=strict;path=/;HttpOnly=true;Secure=true", "csrf=null;samesite=strict;path=/;HttpOnly=true"]);
+    try {
+        const authKeyName = request.cookies.filter((cookie) => cookie.match(/datasquirel_.*_auth_key/))[0];
+        const csrfName = request.cookies.filter((cookie) => cookie.match(/datasquirel_.*_csrf/))[0];
+
+        response.setHeader("Set-Cookie", [`${authKeyName}=null;samesite=strict;path=/;HttpOnly=true;Secure=true`, `${csrfName}=null;samesite=strict;path=/;HttpOnly=true`, `dsqluid=null;samesite=strict;path=/;HttpOnly=true`]);
+
+        /** ********************************************** */
+        /** ********************************************** */
+        /** ********************************************** */
+
+        return {
+            success: true,
+            payload: "User Logged Out",
+        };
+
+        /** ********************************************** */
+        /** ********************************************** */
+        /** ********************************************** */
+    } catch (error) {
+        console.log(error.message);
+
+        return {
+            success: false,
+            payload: "Logout Failed",
+        };
+    }
 
     /** ********************************************** */
     /** ********************************************** */
     /** ********************************************** */
-
-    return {
-        success: true,
-        payload: "User Logged Out",
-    };
 };
 
 /** ********************************************** */
