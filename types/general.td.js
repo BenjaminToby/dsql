c02feeca08e3825f54418766f22582664d98ad0e
Tben <benoti.san@gmail.com>
2023-08-07 05:17:08 +0100

    Type Checking

---
+++ types/general.td.js
@@ -0,0 +1,11 @@
+const http = require("http");
+
+/**
+ * @typedef {http.IncomingMessage} Request
+ */
+
+/**
+ * @typedef {http.ServerResponse} Response
+ */
+
+module.exports = { Request, Response };
