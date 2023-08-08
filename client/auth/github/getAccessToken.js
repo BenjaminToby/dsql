1f9990f80e2247f47a508a1b5d3a904bd33a3011
Tben <benoti.san@gmail.com>
2023-08-08 15:27:45 +0100

    Updates

--- client/auth/github/getAccessToken.js
+++ client/auth/github/getAccessToken.js
@@ -12,11 +12,10 @@
  * @param {string} params.redirectUrl - Github Redirect URL as listed in your oauth app settings: {@link https://datasquirel.com/docs}
  * @param {function(boolean): void} [params.setLoading] - React setState Function: sets whether the google login button is ready or not
  * @param {string[]} [params.scopes] - Scopes to be requested from the user
- * @param {{key: string, value: string}[] | string} [params.extraUriParams] - Extra params to be added to the url
  *
  * @returns {void} - Return
  */
-module.exports = function getAccessToken({ clientId, redirectUrl, setLoading, scopes, extraUriParams }) {
+module.exports = function getAccessToken({ clientId, redirectUrl, setLoading, scopes }) {
     /**
      * == Initialize
      *
@@ -25,18 +24,7 @@ module.exports = function getAccessToken({ clientId, redirectUrl, setLoading, sc
     if (setLoading) setLoading(true);
 
     const scopeString = scopes ? scopes.join("%20") : "read:user";
-
-    const extraUriParamsString = extraUriParams
-        ? typeof extraUriParams == "string"
-            ? extraUriParams
-            : `?${Object.keys(extraUriParams)
-                  .map((key) => `${key}=${extraUriParams[key]}`)
-                  .join("&")}`
-        : "";
-
-    console.log("extraUriParamsString:", extraUriParamsString);
-
-    const fetchUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scopeString}&redirect_uri=${redirectUrl}${extraUriParamsString}`;
+    const fetchUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scopeString}&redirect_uri=${redirectUrl}`;
     window.location.assign(fetchUrl);
 
     //////////////////////////////////////////////////////////////////////////////////
