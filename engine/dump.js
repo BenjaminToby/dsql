cc65ea23b848eefdb0671a68d0b51205fe9454b1
Tben <benoti.san@gmail.com>
2023-08-12 17:07:50 +0100

    Upgrades

--- engine/dump.js
+++ engine/dump.js
@@ -12,7 +12,9 @@ require("dotenv").config({
 const mysqlPath = process.platform?.match(/win/i) ? "'" + "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql.exe" + "'" : "mysql";
 const mysqlDumpPath = process.platform?.match(/win/i) ? "'" + "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe" + "'" : "mysqldump";
 
-const dbName = process.env.DSQL_DB_NAME || "";
+const { DSQL_HOST, DSQL_USER, DSQL_PASS, DSQL_DB_NAME, DSQL_KEY, DSQL_REF_DB_NAME, DSQL_FULL_SYNC, DSQL_ENCRYPTION_KEY, DSQL_ENCRYPTION_SALT } = process.env;
+
+const dbName = DSQL_DB_NAME || "";
 const dumpFilePathArg = process.argv.indexOf("--file");
 
 if (dumpFilePathArg < 0) {
@@ -27,6 +29,11 @@ if (!dbName?.match(/./)) {
     process.exit();
 }
 
+if (!DSQL_USER?.match(/./) || !DSQL_PASS?.match(/./)) {
+    console.log("DSQL_USER and DSQL_PASS are required in your `.env` file");
+    process.exit();
+}
+
 try {
     let execSyncOptions = {
         cwd: process.cwd(),
@@ -34,7 +41,7 @@ try {
 
     if (process.platform.match(/win/i)) execSyncOptions.shell = "bash.exe";
 
-    const dump = execSync(`${mysqlPath} -u ${process.env.DB_USERNAME} -p${process.env.DB_PASSWORD} ${dbName} < ${dumpFilePath}`, execSyncOptions);
+    const dump = execSync(`${mysqlPath} -u ${DSQL_USER} -p${DSQL_PASS} ${dbName} < ${dumpFilePath}`, execSyncOptions);
 
     console.log("Dumped successfully", dump.toString());
 
