2fc92daa94cbbb6197c20abf40d7e435489546bd
Tben <benoti.san@gmail.com>
2023-08-12 17:03:48 +0100

    Upgrades

--- engine/dump.js
+++ engine/dump.js
@@ -9,8 +9,6 @@ require("dotenv").config({
     path: path.resolve(process.cwd(), ".env"),
 });
 
-const varDatabaseDbHandler = require("./engine/utils/varDatabaseDbHandler");
-
 const mysqlPath = process.platform?.match(/win/i) ? "'" + "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysql.exe" + "'" : "mysql";
 const mysqlDumpPath = process.platform?.match(/win/i) ? "'" + "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe" + "'" : "mysqldump";
 
@@ -29,26 +27,22 @@ if (!dbName?.match(/./)) {
     process.exit();
 }
 
-varDatabaseDbHandler({
-    queryString: `CREATE DATABASE \`${dbName}\` IF NOT EXISTS CHARACTER SET utf8mb4 COLLATE utf8mb4_bin`,
-    database: dbName,
-}).then((res) => {
-    console.log("Database creation attempt completed =>", res);
-    try {
-        let execSyncOptions = {
-            cwd: process.cwd(),
-        };
+try {
+    let execSyncOptions = {
+        cwd: process.cwd(),
+    };
 
-        if (process.platform.match(/win/i)) execSyncOptions.shell = "bash.exe";
+    if (process.platform.match(/win/i)) execSyncOptions.shell = "bash.exe";
 
-        execSync(`${mysqlPath} -u ${process.env.DB_USERNAME} -p${process.env.DB_PASSWORD} ${dbName} < ${dumpFilePath}`, execSyncOptions);
+    const dump = execSync(`${mysqlPath} -u ${process.env.DB_USERNAME} -p${process.env.DB_PASSWORD} ${dbName} < ${dumpFilePath}`, execSyncOptions);
 
-        console.log("Dumped successfully");
+    console.log("Dumped successfully", dump.toString());
 
-        ////////////////////////////////////////
-        ////////////////////////////////////////
-        ////////////////////////////////////////
-    } catch (error) {
-        console.log("Dump Error: ", error.message);
-    }
-});
+    console.log("Dumped successfully");
+
+    ////////////////////////////////////////
+    ////////////////////////////////////////
+    ////////////////////////////////////////
+} catch (error) {
+    console.log("Dump Error: ", error.message);
+}
