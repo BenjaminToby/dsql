5e5b541918ec344551dafcdce7d613dd5e64153e
Benjamin Toby <benoti.san@gmail.com>
2024-07-13 08:31:10 +0100

    Update types

--- types/user.td.js
+++ types/user.td.js
@@ -1,15 +1,37 @@
 /**
  * @typedef {object} DATASQUIREL_LoggedInUser
- * @property {number} id - user id (number)
- * @property {string} first_name - User First Name
- * @property {string} last_name - User Last Name
- * @property {string} image - User Full Image
- * @property {string} image_thumbnail - User Image Thumbnail
- * @property {string} [social_id] - User Social id if available
- * @property {number} social_login - 0 or 1 => is this user a social user(1) or not(0)
- * @property {string} csrf_k - CSRF key
- * @property {boolean} logged_in_status - Is user logged in or not
- * @property {boolean} [date] - Time of session creation
+ * @property {number} [id]
+ * @property {string} first_name
+ * @property {string} last_name
+ * @property {string} email
+ * @property {string} [phone]
+ * @property {string} [user_type]
+ * @property {string} [username]
+ * @property {string} password
+ * @property {string} [image]
+ * @property {string} [image_thumbnail]
+ * @property {string} [address]
+ * @property {string} [city]
+ * @property {string} [state]
+ * @property {string} [country]
+ * @property {string} [zip_code]
+ * @property {number} [social_login]
+ * @property {string?} [social_platform]
+ * @property {string?} [social_id]
+ * @property {string} [more_user_data]
+ * @property {number} [verification_status]
+ * @property {number?} [loan_officer_id]
+ * @property {number} [is_admin]
+ * @property {number} [admin_level]
+ * @property {string} [admin_permissions]
+ * @property {string?} uuid
+ * @property {string?} [temp_login_code]
+ * @property {string?} [date_created]
+ * @property {number?} [date_created_code]
+ * @property {string?} [date_created_timestamp]
+ * @property {string?} [date_updated]
+ * @property {number?} [date_updated_code]
+ * @property {string?} [date_updated_timestamp]
  */
 
 module.exports = { DATASQUIREL_LoggedInUser };
