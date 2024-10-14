6a358313cb2c13d7069c1b2f6946c4613adc2fb0
Benjamin Toby <benoti.san@gmail.com>
2024-10-14 09:14:20 +0100

    Updates

--- push.sh
+++ push.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+root_dir=/media/benoti/Ubuntu-Disk/Github/dsql
+
 if [ -z "$1" ]; then
     msg="Updates"
 else
     msg="$1"
 fi
+
+cp "$root_dir/package-shared/types/*" "$root_dir/types/*"
 
 git add . && git commit -m "$msg" && git push
